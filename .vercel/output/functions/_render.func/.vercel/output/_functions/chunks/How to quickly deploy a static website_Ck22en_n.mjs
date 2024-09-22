94f49040f69642fb75a5948695dc002b27acec32
Akshay <93368968+akshy3@users.noreply.github.com>
2024-09-22 23:29:00 +0530

    Delete .vercel directory

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/How to quickly deploy a static website_Ck22en_n.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./How to quickly deploy a static website_Dz_P8gr1.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
